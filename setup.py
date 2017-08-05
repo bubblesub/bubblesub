e530f144fc5e18d4d5910b431eb460da5f083317
rr- <rr-@sakuya.pl>
2017-08-05 11:58:13 +0200

    build: add setup.py; remove requirements.txt

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+    author='Marcin Kurczewski',
+    author_email='rr-@sakuya.pl',
+    name='bubblesub',
+    long_description='ASS subtitle editor',
+    version='0.0',
+    url='https://github.com/rr-/bubblesub',
+    packages=find_packages(),
+
+    entry_points={
+        'console_scripts': [
+            'bubblesub = bubblesub.__main__:main'
+        ]
+    },
+
+    install_requires=[
+        'ffms',
+        'numpy',
+        'scipy',
+        'pysubs2',
+        'pyfftw',
+        'PyQT5',
+    ],
+
+    classifiers=[
+        'Environment :: X11 Applications :: Qt',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Text Editors',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Multimedia :: Video',
+    ])
