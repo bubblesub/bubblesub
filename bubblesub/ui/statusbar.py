3895ae8367cf2b0b5a1ff81d2079cd765f8b979b
rr- <rr-@sakuya.pl>
2017-09-04 00:03:33 +0200

    ui: rework status bar

--- bubblesub/ui/statusbar.py
+++ bubblesub/ui/statusbar.py
@@ -6,17 +6,42 @@ class StatusBar(QtWidgets.QStatusBar):
     def __init__(self, api, parent):
         super().__init__(parent)
         self._api = api
-        self.video_frame_label = QtWidgets.QLabel(self)
-        self.audio_selection_label = QtWidgets.QLabel(self)
+        self._subs_label = QtWidgets.QLabel(self)
+        self._video_frame_label = QtWidgets.QLabel(self)
+        self._audio_selection_label = QtWidgets.QLabel(self)
+        self.setSizeGripEnabled(False)
 
-        self.addPermanentWidget(self.video_frame_label)
-        self.addPermanentWidget(self.audio_selection_label)
+        for label in [
+            self._subs_label,
+            self._video_frame_label,
+            self._audio_selection_label
+        ]:
+            label.setFrameStyle(
+                QtWidgets.QFrame.Panel | QtWidgets.QFrame.Sunken)
+            label.setLineWidth(1)
+
+        self.addPermanentWidget(self._subs_label)
+        self.addPermanentWidget(self._video_frame_label)
+        self.addPermanentWidget(self._audio_selection_label)
 
         api.video.current_pts_changed.connect(self._video_current_pts_changed)
         api.audio.selection_changed.connect(self._audio_selection_changed)
+        api.subs.selection_changed.connect(self._subs_selection_changed)
+
+    def _subs_selection_changed(self):
+        idx = (
+            self._api.subs.selected_indexes[0]
+            if self._api.subs.has_selection
+            else None)
+
+        self._subs_label.setText(
+            'Subtitles: {}/{} ({:.1%})'.format(
+                idx if idx is not None else '-',
+                len(self._api.subs.lines),
+                (idx / max(1, len(self._api.subs.lines)) if idx is not None else 0)))
 
     def _video_current_pts_changed(self):
-        self.video_frame_label.setText(
+        self._video_frame_label.setText(
             'Video frame: {} ({:.1%})'.format(
                 bubblesub.util.ms_to_str(self._api.video.current_pts),
                 self._api.video.current_pts / max(1, self._api.video.max_pts)))
@@ -28,7 +53,7 @@ class StatusBar(QtWidgets.QStatusBar):
         start_delta = self._api.audio.selection_start - sub.start
         end_delta = self._api.audio.selection_end - sub.end
 
-        self.audio_selection_label.setText(
+        self._audio_selection_label.setText(
             'Audio selection: {:+.0f} ms / {:+.0f} ms (duration: {})'.format(
                 start_delta,
                 end_delta,
