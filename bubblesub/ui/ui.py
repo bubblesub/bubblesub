c6788384e0734e20e0ae37c64fe6a0a065effd1c
rr- <rr-@sakuya.pl>
2017-09-03 15:15:13 +0200

    ui: remove exception hook

--- bubblesub/ui/ui.py
+++ bubblesub/ui/ui.py
@@ -1,36 +1,8 @@
 import asyncio
 import sys
-import time
-import traceback
-import io
 import bubblesub.ui.main_window
-import bubblesub.ui.util
 import quamash
 from PyQt5 import QtWidgets
-
-
-def excepthook(exception_type, exception_value, traceback_object):
-    separator = '-' * 80
-
-    with io.StringIO() as handle:
-        traceback.print_tb(traceback_object, None, handle)
-        handle.seek(0)
-        traceback_info = handle.read()
-
-    msg = '\n'.join([
-        'An unhandled exception occurred.',
-        time.strftime("%Y-%m-%d, %H:%M:%S"),
-        separator,
-        '%s: \n%s' % (str(exception_type), str(exception_value)),
-        separator,
-        traceback_info
-    ])
-
-    print(msg)
-    bubblesub.ui.util.error(msg)
-
-
-sys.excepthook = excepthook
 
 
 class Ui:
