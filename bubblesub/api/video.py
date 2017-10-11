c4448d9bbd37a7e9f917f7c342ca727067052884
rr- <rr-@sakuya.pl>
2017-10-11 23:05:10 +0200

    api/video: faster no-op seeks

--- bubblesub/api/video.py
+++ bubblesub/api/video.py
@@ -160,10 +160,11 @@ class VideoApi(QtCore.QObject):
         self._set_end(None)  # mpv refuses to seek beyond --end
         pts = max(0, pts)
         pts = self._align_pts_to_next_frame(pts)
-        self._mpv.command(
-            'seek',
-            bubblesub.util.ms_to_str(pts),
-            'absolute+exact' if precise else 'absolute')
+        if pts != self.current_pts:
+            self._mpv.command(
+                'seek',
+                bubblesub.util.ms_to_str(pts),
+                'absolute+exact' if precise else 'absolute')
 
     def step_frame_forward(self):
         if not self._mpv_ready:
