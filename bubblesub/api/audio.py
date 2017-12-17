3011dbdcf8c6aaaffe70ce2bd9de7afdec83d087
rr- <rr-@sakuya.pl>
2017-12-17 13:58:59 +0100

    api/audio: don't crash for corrupt videos

--- bubblesub/api/audio.py
+++ bubblesub/api/audio.py
@@ -202,7 +202,7 @@ class AudioApi(QtCore.QObject):
                 return np.zeros(count).reshape(
                     (count, max(1, self.channel_count)))
             if start_frame + count > self.sample_count:
-                count = self.sample_count - start_frame
+                count = max(0, self.sample_count - start_frame)
             self._audio_source.init_buffer(count)
             return self._audio_source.get_audio(start_frame)
 
