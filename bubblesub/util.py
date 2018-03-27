a36752a1bf4ade455bbdeb805dc5462ae1d4b606
rr- <rr-@sakuya.pl>
2018-03-27 09:40:45 +0200

    util: remove unused method

--- bubblesub/util.py
+++ bubblesub/util.py
@@ -1,11 +1,7 @@
 import re
-import sys
 import time
 import fractions
 import hashlib
-
-from numbers import Number
-from collections import Set, Mapping, deque
 
 import pysubs2.time
 
@@ -56,34 +52,6 @@ class Benchmark:
     def mark(self, msg):
         print('{}: {:.02f} s'.format(msg, time.time() - self._time))
         self._time = time.time()
-
-
-# snippet by Aaron Hall, taken from https://stackoverflow.com/a/30316760
-# CC-BY-SA 3.0
-def getsize(top_obj):
-    visited = set()
-
-    def inner(obj):
-        obj_id = id(obj)
-        if obj_id in visited:
-            return 0
-        visited.add(obj_id)
-        size = sys.getsizeof(obj)
-        if isinstance(obj, (str, bytes, Number, range, bytearray)):
-            pass
-        elif isinstance(obj, (tuple, list, Set, deque)):
-            size += sum(inner(i) for i in obj)
-        elif isinstance(obj, Mapping) or hasattr(obj, 'items'):
-            size += sum(inner(k) + inner(v) for k, v in obj.items())
-        if hasattr(obj, '__dict__'):
-            size += inner(vars(obj))
-        if hasattr(obj, '__slots__'):
-            size += sum(
-                inner(getattr(obj, s))
-                for s in obj.__slots__ if hasattr(obj, s))
-        return size
-
-    return inner(top_obj)
 
 
 def eval_expr(expr):
