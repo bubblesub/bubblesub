ea8d41febd7c9920778656103b7989bd8365882b
Marcin Kurczewski <dash@wind.garden>
2025-08-11 23:50:15 +0200

    docker: add pulseaudio support; improve keyframe seeks

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,9 +3,17 @@
 USER_ID=${LOCAL_USER_ID:-9001}
 echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+adduser user pulse-access
 export HOME=/home/user
 
 # change ownership of the installed app to the user
 chown -R ${USER_ID}:${USER_ID} .
 
-exec gosu user sh -c "uv run --project /home/user/bubblesub bubblesub"
+echo "default-server = unix:/tmp/pulseaudio.socket
+# Prevent a server running in the container
+autospawn = no
+daemon-binary = /bin/true
+# Prevent the use of shared memory
+enable-shm = false" >/etc/pulse/client.conf
+
+exec gosu user sh -c "uv run --no-sync --project /home/user/bubblesub bubblesub"
